bf9085fa1025dde2cd287b998d81898679b15b3d
Chris Guest <chrisguest75@users.noreply.github.com>
2024-10-25 11:56:02 +0100

    test(ctranslate2): :test_tube: Add a test for ctranslate2 so I can test my built version

---
+++ 38_building_packages/ctranslate2/test_ctranslate2/README.md
@@ -0,0 +1,101 @@
+# test_ctranslate2
+
+Demonstrate test_ctranslate2
+
+## Contents
+
+- [test\_ctranslate2](#test_ctranslate2)
+  - [Contents](#contents)
+  - [Prepare](#prepare)
+  - [Start](#start)
+  - [Building](#building)
+  - [Docker](#docker)
+  - [Debugging and Troubleshooting](#debugging-and-troubleshooting)
+    - [Interpreter](#interpreter)
+    - [Pipenv Environment](#pipenv-environment)
+    - [Single step](#single-step)
+      - [Application](#application)
+      - [Tests](#tests)
+  - [Resources](#resources)
+
+## Prepare
+
+If using `vscode` remember to set your interpreter location to `.venv/bin/python`
+
+## Start
+
+```sh
+# for vscode
+export PIPENV_IGNORE_VIRTUALENVS=1
+
+export PIPENV_VENV_IN_PROJECT=1
+# install
+pipenv install --dev
+
+# lint and test code
+pipenv run format
+pipenv run lint
+pipenv run test
+
+# enter venv
+pipenv shell
+
+# create .env file
+cp .env.template .env
+
+# run with arguments
+pipenv run start --test
+pipenv run start:test
+```
+
+## Building
+
+```sh
+curl -o model/transformer-ende-wmt-pyOnmt.tar.gz https://s3.amazonaws.com/opennmt-models/transformer-ende-wmt-pyOnmt.tar.gz
+tar xf transformer-ende-wmt-pyOnmt.tar.gz
+
+pipenv run ct2-opennmt-py-converter --model_path ./model/averaged-10-epoch.pt --output_dir ./model/ende_ctranslate2
+```
+
+## Docker
+
+```sh
+pipenv run docker:build       
+pipenv run docker:start   
+
+# troubleshooting    
+docker run -it --entrypoint /bin/bash test_ctranslate2
+```
+
+## Debugging and Troubleshooting
+
+### Interpreter
+
+Set the interpreter path to `./test_ctranslate2/.venv/bin/python3.11`
+
+### Pipenv Environment
+
+```sh
+# enter python
+pipenv run python
+
+> import main
+
+> main.test.__doc__
+```
+
+### Single step
+
+#### Application
+
+- Copy the `launch.json` to the root `.vscode`
+- `. ./.env` in the terminal
+
+#### Tests
+
+- Configure pytest using the beaker icon in `vscode`
+- You can run and debug the discovered tests
+
+## Resources
+
+- Python testing in Visual Studio Code [here](https://code.visualstudio.com/docs/python/testing#_example-test-walkthroughs)
---
+++ 38_building_packages/ctranslate2/test_ctranslate2/main.py
@@ -0,0 +1,101 @@
+import argparse
+import io
+import logging
+import logging.config
+import sys
+import traceback
+import yaml
+import os
+import platform
+import timeit
+import ctranslate2
+import sentencepiece as spm
+
+def log_uncaught_exceptions(exc_type, exc_value, exc_traceback):
+    """catches unhandled exceptions and logs them"""
+
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+
+    logging.critical("Exception", exc_info=(exc_type, exc_value, exc_traceback))
+    logging.critical(
+        "Unhandled Exception {0}: {1}".format(exc_type, exc_value),
+        extra={"exception": "".join(traceback.format_tb(exc_traceback))},
+    )
+
+def details() -> str:
+    """ return details about python version and platform as a dict """
+    return {
+        "python_version": sys.version,
+        "platform": sys.platform,
+        "platform_details": platform.platform(),
+    }
+
+
+def str2bool(value: str) -> bool:
+    """ converts strings representing truth to bool """ ""
+    return value.lower() in ("yes", "true", "t", "1")
+
+def translate() -> None:
+    translator = ctranslate2.Translator("./model/ende_ctranslate2/", device="cpu")
+    sp = spm.SentencePieceProcessor("./model/sentencepiece.model")
+
+    input_text = "Hello world!"
+    input_tokens = sp.encode(input_text, out_type=str)
+
+    results = translator.translate_batch([input_tokens])
+
+    output_tokens = results[0].hypotheses[0]
+    output_text = sp.decode(output_tokens)
+
+    print(output_text)
+
+
+def test() -> int:
+    """test function"""
+    logger = logging.getLogger()
+    test_config = os.environ["TEST_CONFIG"]
+    logger.info(f'Invoked test function - TEST_CONFIG={test_config!r}')
+    logger.info(f"details={details()}")
+
+    execution_time = timeit.timeit('translate()', globals=globals(), number=100)
+    logger.info(f"Execution time: {execution_time:.4f} seconds")
+
+
+    return 0
+
+
+def main() -> int:
+    """
+    main function
+
+    returns 0 on success, 1 on failure
+
+    configures logging and processes command line arguments
+    """
+    with io.open(
+        f"{os.path.dirname(os.path.realpath(__file__))}/logging_config.yaml"
+    ) as f:
+        logging_config = yaml.load(f, Loader=yaml.FullLoader)
+
+    logging.config.dictConfig(logging_config)
+
+    sys.excepthook = log_uncaught_exceptions
+
+    parser = argparse.ArgumentParser(description="CLI Skeleton")
+    parser.add_argument("--test", dest="test", action="store_true")
+    args = parser.parse_args()
+
+    success = 0
+    if args.test:
+        success = test()
+    else:
+        parser.print_help()
+
+    return success
+
+
+if __name__ == "__main__":
+    # print(f"Enter {__name__}")
+    exit(main())
