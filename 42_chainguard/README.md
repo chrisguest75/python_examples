2366d861a02459254a6e389c61684e55177b36f3
Christopher Guest <chrisguest75@users.noreply.github.com>
2024-10-25 16:18:45 +0000

    feat(chainguard): :sparkles: Build images using chainguard

--- 42_chainguard/README.md
+++ 42_chainguard/README.md
@@ -1,10 +1,19 @@
 # CHAINGUARD
 
+Use chainguard to build images.  
+
+## Build
+
 ```sh
 docker build . -t octo-facts
+```
 
+## Run
+
+```sh
 docker run --rm octo-facts
-
 ```
 
 ## Resources
+
+- https://edu.chainguard.dev/chainguard/chainguard-images/getting-started/python/
--- 42_chainguard/main.py
+++ 42_chainguard/main.py
@@ -1,5 +1,6 @@
 '''Import random module to implement random.choice() function'''
 import random
+from climage import convert
 
 
 def random_line(text):
@@ -12,7 +13,12 @@ def random_line(text):
 def main():
     '''Prints random line from facts.txt; verify your path'''
     print(random_line('facts.txt'))
+    '''Take in PNG and output as ANSI to terminal'''
+    output = convert('linky.png', is_unicode=True)
+    print(output)
 
 
 if __name__ == "__main__":
     main()
+
+
